f189648319210b1d5ab2eb532b5fca100fb23b28
MoXyLe <roofusmoxy@gmail.com>
2019-12-05 16:55:34 +0700

    Rewards

--- updatedb.py
+++ updatedb.py
@@ -1,7 +1,7 @@
 from main.models import Cosmetic
 import codecs
 
-file = codecs.open("C:\\Users\\roofu\\Downloads\\Silver_Battle_Pass.txt", "r", "utf_8_sig")
+file = codecs.open("C:\\Users\\roofu\\Downloads\\Fails_05_12_19(1).txt", "r", "utf_8_sig")
 
 text = file.readlines()
 
@@ -10,12 +10,11 @@ for i in text:
     if i != "":
         type = i.split(":")[0]
         str_name = i.split(":")[1]
-        name = str_name.split(".")[0]
-        print(type)
-        print(name[1:])
+        price = i.split(":")[2].split(".")[0]
+        print(price[1:] + " price")
         try:
-            a = Cosmetic.objects.get(name=name[1:], short_description=type)
-            a.free_pass = True
+            a = Cosmetic.objects.get(name=str_name, short_description=type)
+            a.source = price[1:]
             a.save()
         except Exception as e:
             print(e)
