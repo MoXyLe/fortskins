da6005b12f28282645396a96b14919372b462640
MoXyLe <roofusmoxy@gmail.com>
2019-12-07 20:51:34 +0700

    _

--- main/views.py
+++ main/views.py
@@ -9,211 +9,232 @@ from django.template.loader import render_to_string
 
 def items(request):
     cosmetics = Cosmetic.objects.order_by("rarity_sort")
-    sort = str()
-    amount = str()
+    type = ""
+    source = ""
+    rarity = ""
+    sort = ""
+    amount = ""
     for i in request.GET:
         if i == "type":
             if request.GET['type'] != 'all':
                 if request.GET['type'] == "skin":
-                    cosmetics = skin(cosmetics)
+                    [type, cosmetics] = skin(cosmetics)
                 elif request.GET['type'] == 'pickaxe':
-                    cosmetics = pickaxe(cosmetics)
+                    [type, cosmetics] = pickaxe(cosmetics)
                 elif request.GET['type'] == 'backpack':
-                    cosmetics = backpack(cosmetics)
+                    [type, cosmetics] = backpack(cosmetics)
                 elif request.GET['type'] == 'pet':
-                    cosmetics = pet(cosmetics)
+                    [type, cosmetics] = pet(cosmetics)
                 elif request.GET['type'] == 'emote':
-                    cosmetics = emote(cosmetics)
+                    [type, cosmetics] = emote(cosmetics)
                 elif request.GET['type'] == 'glider':
-                    cosmetics = glider(cosmetics)
+                    [type, cosmetics] = glider(cosmetics)
                 elif request.GET['type'] == 'wrap':
-                    cosmetics = wrap(cosmetics)
+                    [type, cosmetics] = wrap(cosmetics)
                 elif request.GET['type'] == 'music':
-                    cosmetics = music(cosmetics)
+                    [type, cosmetics] = music(cosmetics)
                 elif request.GET['type'] == 'trail':
-                    cosmetics = trail(cosmetics)
+                    [type, cosmetics] = trail(cosmetics)
         elif i == "source":
             if request.GET['source'] != 'all':
                 if request.GET['source'] == 'pack':
-                    cosmetics = pack(cosmetics)
+                    [source, cosmetics] = pack(cosmetics)
                 elif request.GET['source'] == 'exclusive':
-                    cosmetics = exclusive(cosmetics)
+                    [source, cosmetics] = exclusive(cosmetics)
                 elif request.GET['source'] == 'shop':
-                    cosmetics = item_shop(cosmetics)
+                    [source, cosmetics] = item_shop(cosmetics)
                 elif request.GET['source'] == 'pass':
-                    cosmetics = pass_all(cosmetics)
+                    [source, cosmetics] = pass_all(cosmetics)
                 elif request.GET['source'] == 'pass1':
-                    cosmetics = pass1(cosmetics)
+                    [source, cosmetics] = pass1(cosmetics)
                 elif request.GET['source'] == 'pass2':
-                    cosmetics = pass2(cosmetics)
+                    [source, cosmetics] = pass2(cosmetics)
                 elif request.GET['source'] == 'pass3':
-                    cosmetics = pass3(cosmetics)
+                    [source, cosmetics] = pass3(cosmetics)
                 elif request.GET['source'] == 'pass4':
-                    cosmetics = pass4(cosmetics)
+                    [source, cosmetics] = pass4(cosmetics)
                 elif request.GET['source'] == 'pass5':
-                    cosmetics = pass5(cosmetics)
+                    [source, cosmetics] = pass5(cosmetics)
                 elif request.GET['source'] == 'pass6':
-                    cosmetics = pass6(cosmetics)
+                    [source, cosmetics] = pass6(cosmetics)
                 elif request.GET['source'] == 'pass7':
-                    cosmetics = pass7(cosmetics)
+                    [source, cosmetics] = pass7(cosmetics)
                 elif request.GET['source'] == 'pass8':
-                    cosmetics = pass8(cosmetics)
+                    [source, cosmetics] = pass8(cosmetics)
                 elif request.GET['source'] == 'pass9':
-                    cosmetics = pass9(cosmetics)
+                    [source, cosmetics] = pass9(cosmetics)
                 elif request.GET['source'] == 'pass10':
-                    cosmetics = pass10(cosmetics)
+                    [source, cosmetics] = pass10(cosmetics)
                 elif request.GET['source'] == 'pass11':
-                    cosmetics = pass11(cosmetics)
+                    [source, cosmetics] = pass11(cosmetics)
         elif i == "rarity":
             if request.GET['rarity'] != 'all':
                 if request.GET['rarity'] == "leg":
-                    cosmetics = leg(cosmetics)
+                    [rarity, cosmetics] = leg(cosmetics)
                 elif request.GET['rarity'] == "epic":
-                    cosmetics = epic(cosmetics)
+                    [rarity, cosmetics] = epic(cosmetics)
                 elif request.GET['rarity'] == "rare":
-                    cosmetics = rare(cosmetics)
+                    [rarity, cosmetics] = rare(cosmetics)
                 elif request.GET['rarity'] == "uncom":
-                    cosmetics = uncom(cosmetics)
+                    [rarity, cosmetics] = uncom(cosmetics)
                 elif request.GET['rarity'] == "common":
-                    cosmetics = common(cosmetics)
+                    [rarity, cosmetics] = common(cosmetics)
                 elif request.GET['rarity'] == "starwars":
-                    cosmetics = starwars(cosmetics)
+                    [rarity, cosmetics] = starwars(cosmetics)
                 elif request.GET['rarity'] == "dc":
-                    cosmetics = dc(cosmetics)
+                    [rarity, cosmetics] = dc(cosmetics)
                 elif request.GET['rarity'] == "star":
-                    cosmetics = star(cosmetics)
+                    [rarity, cosmetics] = star(cosmetics)
                 elif request.GET['rarity'] == "evil":
-                    cosmetics = evil(cosmetics)
+                    [rarity, cosmetics] = evil(cosmetics)
                 elif request.GET['rarity'] == "marvel":
-                    cosmetics = marvel(cosmetics)
+                    [rarity, cosmetics] = marvel(cosmetics)
                 elif request.GET['rarity'] == "ghost":
-                    cosmetics = ghost(cosmetics)
+                    [rarity, cosmetics] = ghost(cosmetics)
                 elif request.GET['rarity'] == "lava":
-                    cosmetics = lava(cosmetics)
+                    [rarity, cosmetics] = lava(cosmetics)
                 elif request.GET['rarity'] == "ice":
-                    cosmetics = ice(cosmetics)
+                    [rarity, cosmetics] = ice(cosmetics)
         elif i == "amount":
             amount = request.GET['amount']
         elif i == "sort":
             sort = request.GET['sort']
+
+    if type == "":
+        type = "Все"
+
+    if source == "":
+        source = "Все"
+
+    if rarity == "":
+        rarity = "Все"
+
     if sort == 'random':
         cosmetics = cosmetics.order_by("?")
-    if amount == '100':
-        cosmetics = cosmetics[:100]
+        sort = "Случайная"
+    else:
+        sort = "По редкости"
+
+    if amount == 'all':
+        amount = "Все"
     elif amount == '500':
         cosmetics = cosmetics[:500]
-    return render(request, 'main.html', {'Cosmetics': cosmetics})
+    else:
+        amount = '100'
+        cosmetics = cosmetics[:100]
+
+    return render(request, 'main.html', {'Cosmetics': cosmetics, 'type': type, 'source': source, 'rarity': rarity, 'amount': amount, 'sort': sort})
 
 def skin(cosmetics):
-    return cosmetics.filter(short_description="Экипировка").order_by("rarity_sort")
+    return ["Экипировка", cosmetics.filter(short_description="Экипировка").order_by("rarity_sort")]
 
 def pickaxe(cosmetics):
-    return cosmetics.filter(short_description="Инструмент").order_by("rarity_sort")
+    return ["Инструмент", cosmetics.filter(short_description="Инструмент").order_by("rarity_sort")]
 
 def backpack(cosmetics):
-    return cosmetics.filter(short_description="Украшение на спину").order_by("rarity_sort")
+    return ["Украшение на спину", cosmetics.filter(short_description="Украшение на спину").order_by("rarity_sort")]
 
 def pet(cosmetics):
-    return cosmetics.filter(short_description="Питомец").order_by("rarity_sort")
+    return ["Питомец", cosmetics.filter(short_description="Питомец").order_by("rarity_sort")]
 
 def emote(cosmetics):
-    return cosmetics.filter(short_description="Эмоция").order_by("rarity_sort")
+    return ["Эмоция", cosmetics.filter(short_description="Эмоция").order_by("rarity_sort")]
 
 def glider(cosmetics):
-    return cosmetics.filter(short_description="Дельтаплан").order_by("rarity_sort")
+    return ["Дельтаплан", cosmetics.filter(short_description="Дельтаплан").order_by("rarity_sort")]
 
 def wrap(cosmetics):
-    return cosmetics.filter(short_description="Обёртка").order_by("rarity_sort")
+    return ["Обёртка", cosmetics.filter(short_description="Обёртка").order_by("rarity_sort")]
 
 def music(cosmetics):
-    return cosmetics.filter(short_description="Музыка").order_by("rarity_sort")
+    return ["Музыка", cosmetics.filter(short_description="Музыка").order_by("rarity_sort")]
 
 def trail(cosmetics):
-    return cosmetics.filter(short_description__istartswith="Воздушный ").order_by("rarity_sort")
+    return ["Воздушный след", cosmetics.filter(short_description__istartswith="Воздушный ").order_by("rarity_sort")]
 
 def pack(cosmetics):
-    return cosmetics.filter(source="Стартовый набор").order_by("rarity_sort")
+    return ["Стартовый набор", cosmetics.filter(source="Стартовый набор").order_by("rarity_sort")]
 
 def exclusive(cosmetics):
-    return cosmetics.filter(source="Эксклюзив").order_by("rarity_sort")
+    return ["Эксклюзив", cosmetics.filter(source="Эксклюзив").order_by("rarity_sort")]
 
 def item_shop(cosmetics):
-    return cosmetics.filter(source="Магазин предметов").order_by("rarity_sort")
+    return ["Магазин предметов", cosmetics.filter(source="Магазин предметов").order_by("rarity_sort")]
 
 def pass_all(cosmetics):
-    return cosmetics.filter(source__contains="Боевой пропуск").order_by("rarity_sort")
+    return ["Все боевые пропуски", cosmetics.filter(source__contains="Боевой пропуск").order_by("rarity_sort")]
 
 def pass1(cosmetics):
-    return cosmetics.filter(source="Боевой пропуск 1 сезона").order_by("rarity_sort")
+    return ["Боевой пропуск 1 сезона", cosmetics.filter(source="Боевой пропуск 1 сезона").order_by("rarity_sort")]
 
 def pass2(cosmetics):
-    return cosmetics.filter(source="Боевой пропуск 2 сезона").order_by("rarity_sort")
+    return ["Боевой пропуск 2 сезона", cosmetics.filter(source="Боевой пропуск 2 сезона").order_by("rarity_sort")]
 
 def pass3(cosmetics):
-    return cosmetics.filter(source="Боевой пропуск 3 сезона").order_by("rarity_sort")
+    return ["Боевой пропуск 3 сезона", cosmetics.filter(source="Боевой пропуск 3 сезона").order_by("rarity_sort")]
 
 def pass4(cosmetics):
-    return cosmetics.filter(source="Боевой пропуск 4 сезона").order_by("rarity_sort")
+    return ["Боевой пропуск 4 сезона", cosmetics.filter(source="Боевой пропуск 4 сезона").order_by("rarity_sort")]
 
 def pass5(cosmetics):
-    return cosmetics.filter(source="Боевой пропуск 5 сезона").order_by("rarity_sort")
+    return ["Боевой пропуск 5 сезона", cosmetics.filter(source="Боевой пропуск 5 сезона").order_by("rarity_sort")]
 
 def pass6(cosmetics):
-    return cosmetics.filter(source="Боевой пропуск 6 сезона").order_by("rarity_sort")
+    return ["Боевой пропуск 6 сезона", cosmetics.filter(source="Боевой пропуск 6 сезона").order_by("rarity_sort")]
 
 def pass7(cosmetics):
-    return cosmetics.filter(source="Боевой пропуск 7 сезона").order_by("rarity_sort")
+    return ["Боевой пропуск 7 сезона", cosmetics.filter(source="Боевой пропуск 7 сезона").order_by("rarity_sort")]
 
 def pass8(cosmetics):
-    return cosmetics.filter(source="Боевой пропуск 8 сезона").order_by("rarity_sort")
+    return ["Боевой пропуск 8 сезона", cosmetics.filter(source="Боевой пропуск 8 сезона").order_by("rarity_sort")]
 
 def pass9(cosmetics):
-    return cosmetics.filter(source="Боевой пропуск 9 сезона").order_by("rarity_sort")
+    return ["Боевой пропуск 9 сезона", cosmetics.filter(source="Боевой пропуск 9 сезона").order_by("rarity_sort")]
 
 def pass10(cosmetics):
-    return cosmetics.filter(source="Боевой пропуск 10 сезона").order_by("rarity_sort")
+    return ["Боевой пропуск 10 сезона", cosmetics.filter(source="Боевой пропуск 10 сезона").order_by("rarity_sort")]
 
 def pass11(cosmetics):
-    return cosmetics.filter(source="Боевой пропуск 11 сезона").order_by("rarity_sort")
+    return ["Боевой пропуск 11 сезона", cosmetics.filter(source="Боевой пропуск 11 сезона").order_by("rarity_sort")]
 
 def leg(cosmetics):
-    return cosmetics.filter(display_rarity="Легендарный")
+    return ["Легендарный", cosmetics.filter(display_rarity="Легендарный")]
 
 def epic(cosmetics):
-    return cosmetics.filter(display_rarity="Эпический")
+    return ["Эпический", cosmetics.filter(display_rarity="Эпический")]
 
 def rare(cosmetics):
-    return cosmetics.filter(display_rarity="Редкий")
+    return ["Редкий", cosmetics.filter(display_rarity="Редкий")]
 
 def uncom(cosmetics):
-    return cosmetics.filter(display_rarity="Необычный")
+    return ["Необычный", cosmetics.filter(display_rarity="Необычный")]
 
 def common(cosmetics):
-    return cosmetics.filter(display_rarity="Обычный")
+    return ["Обычный", cosmetics.filter(display_rarity="Обычный")]
 
 def starwars(cosmetics):
-    return cosmetics.filter(display_rarity="Серия Звездные Войны")
+    return ["Серия Звездные Войны", cosmetics.filter(display_rarity="Серия Звездные Войны")]
 
 def dc(cosmetics):
-    return cosmetics.filter(display_rarity="Серия DC")
+    return ["Серия DC", cosmetics.filter(display_rarity="Серия DC")]
 
 def star(cosmetics):
-    return cosmetics.filter(display_rarity="Звёздная серия")
+    return ["Звёздная серия", cosmetics.filter(display_rarity="Звёздная серия")]
 
 def evil(cosmetics):
-    return cosmetics.filter(display_rarity="Зловещая серия")
+    return ["Зловещая серия", cosmetics.filter(display_rarity="Зловещая серия")]
 
 def marvel(cosmetics):
-    return cosmetics.filter(display_rarity="Серия Марвел")
+    return ["Серия Марвел", cosmetics.filter(display_rarity="Серия Марвел")]
 
 def ghost(cosmetics):
-    return cosmetics.filter(display_rarity="Призрачная серия")
+    return ["Призрачная серия", cosmetics.filter(display_rarity="Призрачная серия")]
 
 def lava(cosmetics):
-    return cosmetics.filter(display_rarity="Лавовая серия")
+    return ["Лавовая серия", cosmetics.filter(display_rarity="Лавовая серия")]
 
 def ice(cosmetics):
-    return cosmetics.filter(display_rarity="Ледяная серия")
+    return ["Ледяная серия", cosmetics.filter(display_rarity="Ледяная серия")]
 
 def shop(request):
     response = requests.get("https://fortnite-api.com/shop/br?language=ru")
