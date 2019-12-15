b73eec8b6c4f17204a2a64ed0b230a3f7a803db4
root <root@fortwhat.com>
2019-12-15 22:39:34 +0300

    Favicon added

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from main import views
+from django.views.generic.base import RedirectView
 
 app_name = 'main'
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     path('', views.items, name="items"),
     path('shop', views.shop, name="shop"),
     path('seacrh', views.search, name="search"),
+    path('favicon.ico', favicon_view),
     path('<str:href>', views.oneskin, name="oneskin"),
 ]
